7c9a345257e52a86854311c6a8d20a50808a755b
nguyenkhanhhn <52152972+nguyenkhanhhn@users.noreply.github.com>
2019-07-22 08:32:10 +0700

    Mã quốc gia

---
+++ Ma nuoc my.js
@@ -0,0 +1,11 @@
+function is_usZipCode(str) {
+    regexp = /^[0-9]{5}(?:-[0-9]{4})?$/;
+    if (regexp.test(str)) {
+        return true;
+    }
+    return false;
+}
+let zipcode = "03201:-150";
+    console.log(is_usZipCode(zipcode));
+zipcode = "7892";
+console.log(is_usZipCode(zipcode));
